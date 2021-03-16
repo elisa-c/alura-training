624060e8230919e047e3da2b14f0404b6eb1b487
Elisa Camillo <elisatcamillo@gmail.com>
2021-03-16 10:53:35 -0300

    :tada: table view with static cells - swift part 2 - 1.3

---
+++ dynamic-meals-table/dynamic-meals-table/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dynamic-meals-table
+//
+//  Created by Elisa Camillo on 16/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
