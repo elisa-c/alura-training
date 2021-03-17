0352ca1f015eff99d2c22428be27cba2278549fb
Elisa Camillo <elisatcamillo@gmail.com>
2021-03-17 00:50:00 -0300

    :art: adding table view

--- eggplant-brownie/eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/eggplant-brownie/Controllers/ViewController.swift
@@ -7,12 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+
+    
+    // IBOutlets
         
     @IBOutlet var nameTextField:UITextField?
     @IBOutlet weak var happinessTextField: UITextField?
     
+    // Attributes
+    
     var delegate: AddMealsDelegate?
+    var items = ["tomato sauce", "cheese", "basil"]
+    
+    // UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let item = items[indexPath.row]
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = item
+        
+        return cell
+    }
+    
+    // IBAction
     
     @IBAction func add(_ sender: Any){ // _  hidden parameter
 
